38dc0c86ca31c2d7d0f6e2b6a6980e633bef49e8
arora-sumit09 <sumitarora1999@gmail.com>
2024-01-03 00:53:24 +0530

    added the local storage too ;

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector('.filter-todo');
 // event listeners
+document.addEventListener('DOMContentLoaded',getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener('click' , deleteCheck);
 filterOption.addEventListener("click" , filterTodo);
@@ -21,6 +22,8 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value; // taking the value of the form
   newTodo.classList.add("todo-item"); // we created a li with an class as todo item now
   todoDiv.appendChild(newTodo);
+  // saving it to local storage
+  saveLocalTodos(todoInput.value);
 
   // now create buttons
   const completedButton = document.createElement("button");
@@ -43,9 +46,15 @@ function deleteCheck(e){
   const item = e.target;
   if(item.classList[0] === 'trash-btn'){
     // todoList.remove(item);
-    const todo = item.parentElement; 
-    todo.remove();
-  
+    const todo = item.parentElement;
+    // adding animation 
+    // todo.classList.add("fall");
+    removeLocalTodos(todo);
+    // todo.addEventListener("transitioned", ()=>{
+      //   todo.remove();
+      // });
+      todo.remove();
+      
   }
   if(item.classList[0] === 'complete-btn'){
     // todoList.remove(item);
@@ -78,4 +87,62 @@ function filterTodo(e){
     }
   
   });
+}
+function checkTodos(){
+  let todos ; 
+  if(localStorage.getItem('todos')===null){
+    todos = [];
+  }
+  else{
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  return todos;
+}
+
+
+function saveLocalTodos(todo){
+  // check for previous todos
+  let todos = checkTodos();
+  todos.push(todo);
+  localStorage.setItem("todos" , JSON.stringify(todos));
+}
+// in the below function what i am doing is -> firstly i am just storing the key value pairs that is todos and corresponding to it the form input value
+// i am not storing an complete element in the list so i have to go to each value i.e already present in the local storage and create the corresponding 
+// div tags and all the above things i have done
+
+function getTodos(){
+  let todos = checkTodos();
+
+  todos.forEach(function(todo){
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo; // taking the value of the form
+    newTodo.classList.add("todo-item"); // we created a li with an class as todo item now
+    todoDiv.appendChild(newTodo);
+    
+  
+    // now create buttons
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+  
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+  
+    //append to ul
+    todoList.appendChild(todoDiv);
+  });
+}
+// for the function below i want to remove the elements i am clicking on from the array
+function removeLocalTodos(todo){
+  let todos = checkTodos();
+  // console.log(todo.children);
+  const todoIndex = todos.indexOf(todo.children[0].innerText);
+  todos.splice(todoIndex,1);
+  localStorage.setItem('todos',JSON.stringify(todos));
 }
